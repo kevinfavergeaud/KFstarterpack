8a2375c1e3bb0361625253c6785629f19fc1e64c
KevinF <kev.favergeaud@gmail.com>
2018-12-29 23:40:50 -0400

    V1.0

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KevinF Start Pack</title>
+
+
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+    <!-- Regular StyleSheets -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/faw.css">
+    <link rel="stylesheet" href="css/app.css">
+
+
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+    <script src="js/jquery.js"></script>
+</body>
+</html>
